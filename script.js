e13c9005664bf5a905d2c26db91c041550860910
Lars Tuerke <127292505+larstrk@users.noreply.github.com>
2024-01-09 21:26:08 +0100

    Initial project commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Basic math functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return b !== 0 ? a / b : "Fehler";
+}
+
