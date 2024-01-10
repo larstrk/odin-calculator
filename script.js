1c8622fa5c2e90e6d0980767e2958b74a4e5cd05
Lars Tuerke <127292505+larstrk@users.noreply.github.com>
2024-01-10 21:08:31 +0100

    UI and start of logic

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+let operator = null;
+let firstNumber = null;
+let secondNumber = null;
+let displayNumber = 0;
+let pressedNumber = null;
+let pressedOperator = null;
+let operatorSelected = false;
+
+
+let display = document.getElementById('display');
+display.textContent = displayNumber;
+
+document.querySelectorAll('.number').forEach(button => {
+    button.addEventListener('click', function() {
+        pressedNumber = this.textContent;
+        console.log(pressedNumber);
+
+        if (displayNumber === 0 || operatorSelected) {
+            displayNumber = pressedNumber;
+            operatorSelected = false;
+        } else  {
+            displayNumber = displayNumber + pressedNumber;
+        }
+        display.textContent = displayNumber;
+        
+    });
+});
+
+
+
+document.querySelectorAll('.operator').forEach(button => {
+    button.addEventListener('click', function() {
+        pressedOperator = this.textContent; // Saves the operator
+        console.log(pressedOperator); 
+        operatorSelected = true; // To start the displayNuber new
+        firstNumber = parseInt(displayNumber, 10); // Save the displayNumber as an Integer in the firstNumber
+        operator = pressedOperator;
+    });
+});
+
+
 // Basic math functions
 function add(a, b) {
     return a + b;
@@ -14,4 +55,3 @@ function multiply(a, b) {
 function divide(a, b) {
     return b !== 0 ? a / b : "Fehler";
 }
-
